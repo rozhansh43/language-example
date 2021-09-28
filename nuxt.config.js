0d77b983da1338d6e0e75ea5b03588a5c2395c20
rozhan <rozhansheikholmoluki@gmail.com>
2021-09-28 10:33:31 +0330

    init files

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+import { API_ROOT, I18N } from './config'
+
+export default {
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#fff' },
+    modules: [
+        ['nuxt-i18n', I18N],
+        [
+            '@nuxtjs/axios',
+            {
+                baseURL: API_ROOT
+            }
+        ]
+    ],
+    plugins: [
+        { src: '~/plugins/version' }
+    ]
+}
